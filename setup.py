8d931426017e3f91bdfeb9458dfb6f4b0ab2a4cb
Abdul Muiz <muizabdul29@gmail.com>
2023-10-13 09:28:44 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='pybatteryid',
-    version='1.0.0',
+    version='1.0.1',
     author='Muiz',
     description='Data-driven Battery Model Identification in Python',
     long_description=readme,
